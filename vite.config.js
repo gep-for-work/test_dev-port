538c9de6bcf7ac2cb03b1102053623660d7f8955
Jenka_Petrovich <gep.for.work@gmail.com>
2022-09-25 23:56:04 +0300

    Try fix for "GitHub Pages".

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
